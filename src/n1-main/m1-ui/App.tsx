c6bd75fb3b1385a3e6b0b5f20892d16fdaf3b6bd
Ekaterina <ekaterinan112@gmail.com>
2021-01-05 16:44:35 +0300

    common component added

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
 import './App.css';
+import UniversalButton from './common/c2-Button/UniversalButton';
+import UniversalCheckbox from './common/c3-Checkbox/UniversalCheckbox';
+import UniversalInputText from './common/c1-InputText/UniversalInputText';
 
 function App() {
-  return (
-    <div className="App">
-//hrouter, provider
-      <>
-        <Main/>
-      </>
-    </div>
-  );
+    return (<div className="App">
+            <UniversalButton/>
+            <UniversalInputText/>
+            <UniversalCheckbox/>
+        </div>
+    );
 }
 
 export default App;
